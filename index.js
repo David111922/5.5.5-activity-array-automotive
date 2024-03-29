4eb1d158fe503f639d53cd45e4312f3adb03ce17
David111922 <salve62@yahoo.com>
2024-03-29 03:33:22 -0700

    changes to array automotive

---
+++ index.js
@@ -0,0 +1,8 @@
+let V = new Car('Cheverolet', 'Silverado', '2005', 'blue', 240000);
+
+V.start();
+V.loadPassenger(5);
+V.stop();
+V.checkService();
+
+console.log(V);
--- mercurySedan.js
+++ mercurySedan.js
@@ -1,17 +1,63 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = Require("./vehicleBaseClass")
+class Car extends VehicleModule{
+    constructor(make, model, year, color, mileage) {
+    super(make, model, year, color, mileage);
+this.maxPassengers = 5;
+this.passenger = 0;
+this.numberOfWheels =4; 
+this.maximumSpeed = 160;
+this.fuel = 10;
+ this.scheduledService = false;
+}
 
-//this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+checkService(){
+if (this.mileage > 30000) {
+    this.scheduledService = true
+    return this.scheduledService;
+}
+}
+
+start() {
+if (this.fuel > 0) {
+ console.log("engine has started")
+ return this.started = true
+}
+ else{ 
+console.log("no fuel");
+return this.started = false;
+
+ }
+}
+loadPassenger(num) {
+    if (this.passenger < this.maxPassengers) {
+        if ((num + this.passenger) <= this.maxPassengers) {
+            this.passenger += num; // Increment passenger count
+            return this.passenger;               
+        } else {
+            console.log(this.model + " " + this.make + " does not have enough space to take all passengers.");
+        }
+    } else {
+        console.log(this.model + " " + this.make + " is full");
+    }
+}
+}
+
+let V = new Car('Mercury', 'Cougar', '2002', 'White', 50000);
+
+V.start();
+V.loadPassenger(5);
+V.stop();
+V.checkService();
+
+console.log(V);
+
+    
 
 
-//After you write the derived Car class, you should test it out.
-
-//Note: You can code your derived Car class here or make a file named index.js and do it there.
 
 
-//TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
+
 
 
 
